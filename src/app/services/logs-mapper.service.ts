9cd0a1a62b6b84288f7c45d979542733f01f3f0d
Bayyr Oorjak <the.bayyr.oorjak@gmail.com>
2024-07-25 15:00:03 +0700

    docs: add comments

--- src/app/services/logs-mapper.service.ts
+++ src/app/services/logs-mapper.service.ts
@@ -5,10 +5,20 @@ import { filterByProduct } from "../helpers/filter-by-product";
 import { filterByStatus } from "../helpers/filter-by-status";
 import { sort } from "../helpers/sort";
 
+/**
+ * This service was supposed to map DTOs into
+ * our own Models but since we use the same interface
+ * this service is no longer needed.
+ * Just kept it for the sake of backend imitation via
+ * `applyFilters` method.
+ */
 @Injectable({
   providedIn: "root",
 })
 export class LogsMapperService {
+  /**
+   * Ideally this should be done on server.
+   */
   applyFilters(logs: Log[], _filter: LogsFilter) {
     const filteredLogs = logs
       .filter((log) => filterByProduct(log, _filter.product))
